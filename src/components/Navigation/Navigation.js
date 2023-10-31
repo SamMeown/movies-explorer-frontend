b521297984c25261192cc2b7a94f0ac73203cc5f
Sam <sammeown@gmail.com>
2023-10-31 15:44:00 +0200

    Header - mobile menu - done

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -13,15 +13,18 @@ function Navigation() {
 
   return (
   <>
-    <nav className="header__menu-unsigned header__navigation">
-      <Link className="header__menu-link header__menu-link_alignment_end" to="/signup">Регистрация</Link>
-      <Link className="header__menu-link header__menu-link_type_button header__menu-link_alignment_end" to="/signin">Войти</Link>
+    <nav className="header__menu-unsigned header__navigation ">
+      <Link className="header__menu-link header__menu-link_alignment_right" to="/signup">Регистрация</Link>
+      <Link className="header__menu-link header__menu-link_type_button header__menu-link_alignment_right" to="/signin">Войти</Link>
     </nav>
     <nav className="header__menu header__navigation header__navigation_visible">
       <button className={`header__menu-btn ${isMenuOpened && 'header__menu-btn_pressed'}`} onClick={handleOpenMenu}>
         <span className="header__menu-btn-icon"></span>
       </button>
-      <div className="meader__menu-popup">
+      <div 
+        className={`header__menu-popup ${isMenuOpened && "header__menu-popup_opened"}`}
+        onClick={evt => evt.target === evt.currentTarget && handleOpenMenu()}
+      >
         <div className="header__menu-box">
           <NavLink 
             className={
@@ -42,7 +45,7 @@ function Navigation() {
             to="/saved-movies"
           >Сохраненные фильмы</NavLink>
           <Link 
-            className="header__menu-link header__menu-link_size_large header__menu-link_type_icon-button header__menu-link_alignment_end"
+            className="header__menu-link header__menu-link_size_large header__menu-link_type_icon-button header__menu-link_alignment_right header__menu-link_alignment_mobile-bottom"
             to="/profile"
           ><span>Аккаунт</span><img src={accountIcon} alt="иконка аккаунта" /></Link>
         </div>

08e21d8ab2d46eb84eabd75242b4e41082595f46
Sam <sammeown@gmail.com>
2023-11-07 17:59:57 +0200

    Movies - SearchForm  - desktop

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,10 +1,27 @@
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
-import './SearchForm.css'
+import './SearchForm.css';
+import searchIcon from '../../images/search-icon.svg';
 
 function SearchForm() {
   return (
     <search className="search movies__search">
-      <FilterCheckbox />
+      <div className="search__form-container">
+        <form className="search__form" name="search-form">
+          <fieldset className="search__fieldset">
+            <div className="search__main-container">
+              <label className="search__request-label">
+                <img className="search__request-icon" src={searchIcon} alt="иконка поиска" />
+                <input className="search__request-input" type="text" name="request" placeholder="Фильм" minlength="2" maxlength="200"/>
+              </label>
+              <button className="search__submit-btn" type="submit">Найти</button>
+            </div>
+            <hr className="search__divider"/>
+            <div className="search__checkbox-container">
+              <FilterCheckbox />
+            </div>
+          </fieldset>
+        </form>
+      </div>
     </search>
   );
 }

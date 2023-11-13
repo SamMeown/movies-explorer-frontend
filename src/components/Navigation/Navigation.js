aa26856bed74dd2d598a2c5dc09a6c4ad7bcf48c
Sam <sammeown@gmail.com>
2023-11-13 18:20:44 +0200

    Header - close popup on link click

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,7 +7,7 @@ function Navigation() {
 
   const [isMenuOpened, setIsMenuOpened] = useState(false);
 
-  function handleOpenMenu() {
+  function handleToggleMenu() {
     setIsMenuOpened(state => !state);
   }
 
@@ -18,34 +18,38 @@ function Navigation() {
       <Link className="header__menu-link header__menu-link_type_button header__menu-link_alignment_right" to="/signin">Войти</Link>
     </nav>
     <nav className="header__menu header__navigation header__navigation_visible">
-      <button className={`header__menu-btn ${isMenuOpened && 'header__menu-btn_pressed'}`} onClick={handleOpenMenu}>
+      <button className={`header__menu-btn ${isMenuOpened && 'header__menu-btn_pressed'}`} onClick={handleToggleMenu}>
         <span className="header__menu-btn-icon"></span>
       </button>
       <div 
         className={`header__menu-popup ${isMenuOpened && "header__menu-popup_opened"}`}
-        onClick={evt => evt.target === evt.currentTarget && handleOpenMenu()}
+        onClick={evt => evt.target === evt.currentTarget && handleToggleMenu()}
       >
         <div className="header__menu-box">
           <NavLink 
             className={
               ({isActive}) => `header__menu-link header__main-link header__menu-link_size_large ${isActive ? "header__menu-link_active": ""}`
             }
+            onClick={handleToggleMenu}
             to="/"
           >Главная</NavLink>
           <NavLink 
             className={
               ({isActive}) => `header__menu-link header__menu-link_size_large ${isActive ? "header__menu-link_active": ""}`
             }
+            onClick={handleToggleMenu}
             to="/movies"
           >Фильмы</NavLink>
           <NavLink 
             className={
               ({isActive}) => `header__menu-link header__menu-link_size_large ${isActive ? "header__menu-link_active": ""}`
             }
+            onClick={handleToggleMenu}
             to="/saved-movies"
           >Сохраненные фильмы</NavLink>
           <Link 
             className="header__menu-link header__menu-link_size_large header__menu-link_type_icon-button header__menu-link_alignment_right header__menu-link_alignment_mobile-bottom"
+            onClick={handleToggleMenu}
             to="/profile"
           ><span>Аккаунт</span><img src={accountIcon} alt="иконка аккаунта" /></Link>
         </div>

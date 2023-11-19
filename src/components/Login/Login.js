924c70d95e924bcd4484edc19ec8aeac62e0e836
Sam <sammeown@gmail.com>
2023-11-19 11:22:16 +0200

    Signing screen - Login screen

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,8 +3,23 @@ import MainWithForm from '../MainWithForm/MainWithForm'
 
 function Login() {
   return (
-    <MainWithForm>
-      
+    <MainWithForm 
+      title="Рады видеть!"
+      buttonTitle="Войти"
+      linkLabel="Ещё не зарегистрированы?"
+      linkText="Регистрация"
+      linkRef="/signup"
+    >
+      <label className="form__field">
+        <span className="form__label">E-mail</span>
+        <input className="form__input" id="email-input" type="email" name="email" placeholder="адрес электронной почты" minLength="3" maxLength="100" required value="pochta@yandex.ru"/>
+        <span className="form-error form__input-error form__input-error_el_email-input"></span>
+      </label>
+      <label className="form__field">
+        <span className="form__label">Пароль</span>
+        <input className="form__input" id="password-input" type="password" name="password" placeholder="пароль (от 6 символов)" minLength="6" maxLength="30" required value=""/>
+        <span className="form-error form__input-error form__input-error_el_password-input"></span>
+      </label>
     </MainWithForm>
   );
 }

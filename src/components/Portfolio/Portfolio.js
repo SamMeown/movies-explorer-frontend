cef47d00a586b8ef5a992fdb1463334e3ee0fc21
Sam <sammeown@gmail.com>
2023-11-29 23:08:48 +0200

    Main - updated links

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,17 +6,17 @@ function Portfolio() {
       <h2 className="portfolio__header">Портфолио</h2>
       <ul className="portfolio__items">
         <li className="portfolio__item">
-          <a className="portfolio__link" href="/">
+          <a className="portfolio__link" href="https://github.com/SamMeown/how-to-learn">
             <span className="portfolio__name">Статичный сайт</span><span className="portfolio__arrow">↗</span>
           </a>
         </li>
         <li className="portfolio__item">
-          <a className="portfolio__link" href="/">
+          <a className="portfolio__link" href="https://sammeown.github.io/russian-travel/">
             <span className="portfolio__name">Адаптивный сайт</span><span className="portfolio__arrow">↗</span>
           </a>
         </li>
         <li className="portfolio__item">
-          <a className="portfolio__link" href="/">
+          <a className="portfolio__link" href="https://sammys.nomoredomainsrocks.ru">
             <span className="portfolio__name">Одностраничное приложение</span><span className="portfolio__arrow">↗</span>
           </a>
         </li>

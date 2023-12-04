45d814db5c640f6631483c792e1c4e769420d758
Sam <sammeown@gmail.com>
2023-12-04 14:56:54 +0200

    Made outer links open in separate windows

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -12,7 +12,7 @@ function AboutMe() {
           <h3 className="about-me__name">Виталий</h3>
           <p className="about-me__title">Фронтенд-разработчик, 30 лет</p>
           <p className="about-me__text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-          <a className="about-me__link" href="https://github.com/SamMeown">Github</a>
+          <a className="about-me__link" href="https://github.com/SamMeown" target="_blank" rel="noreferrer">Github</a>
         </div>
         <img className="about-me__photo" src={photo} alt="фото студента" />
       </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,10 +8,10 @@ function Footer() {
         <p className="footer__year">{`© ${new Date().getFullYear()}`}</p>
         <ul className="footer__link-items">
           <li className="footer__link-item">
-            <a className="footer__link" href="https://practicum.yandex.ru">Яндекс.Практикум</a>
+            <a className="footer__link" href="https://practicum.yandex.ru" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
           </li>
           <li className="footer__link-item">
-            <a className="footer__link" href="https://github.com/SamMeown">Github</a>
+            <a className="footer__link" href="https://github.com/SamMeown" target="_blank" rel="noreferrer">Github</a>
           </li>
         </ul>
       </div>
